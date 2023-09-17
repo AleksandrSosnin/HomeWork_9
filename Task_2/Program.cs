b89eb99f9d99e85fec5e1e11d0787d8520451c5e
Aleksandr <sosnin93@yandex.ru>
2023-09-17 19:37:07 +0300

    Task2

---
+++ Task_2/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+
+int InputNum(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()!);
+}
+
+int sum = 0;
+void PrintNumbers(int num1, int num2)
+{
+for (int i = num1; i <= num2; i++)
+{
+    sum += i;
+}
+
+Console.WriteLine("Сумма натуральных чисел от " + num1 + " до " + num2 + " равна " + sum);
+}
+
+int firstNum = InputNum("Введите первое число: ");
+int secondNum = InputNum("Введите второе число: ");
+PrintNumbers(firstNum, secondNum);
